fc4b0b9963a6cf9bbbe2938fdbf61c569046eaaa
abeezer <abeezergolwala98@gmail.com>
2023-01-20 12:43:39 +0530

    App Inbox commit

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
-import React, { Component } from "react";
+import React from 'react';
+import type {Node} from 'react';
 import {
-  Button,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
   Text,
+  Button,
   useColorScheme,
   View,
+  Linking,
 } from 'react-native';
 
 import {
@@ -14,36 +20,10 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 const CleverTap = require('clevertap-react-native');
+CleverTap.setDebugLevel(3);
 CleverTap.initializeInbox();
-CleverTap.addListener(CleverTap.CleverTapInboxDidInitialize, (event) => {
-  _handleCleverTapInbox(CleverTap.CleverTapInboxDidInitialize, event);
-});
-function _handleCleverTapInbox(eventName, event) {
-  console.log('CleverTap Inbox Event - ', eventName, event);
-
-}
-
-//Listener triggered after a new app inbox message is recieved.
-
-CleverTap.addListener(CleverTap.CleverTapInboxMessagesDidUpdate, (event) => {
-  _handleCleverTapInbox(CleverTap.CleverTapInboxMessagesDidUpdate, event);
-  CleverTap.getInboxMessageUnreadCount((err, res) => {
-    console.log('Unread Messages: ', res, err);
-});
-});
-
-CleverTap.createNotificationChannel("abtest","abtest","Clevertap test",3,true);
-
-CleverTap.addListener(CleverTap.CleverTapDisplayUnitsLoaded, (event) => {
-  console.log("CleverTapDisplayUnitsLoaded",event);
-});
-CleverTap.addListener(CleverTap.CleverTapInAppNotificationButtonTapped, (event) => {
-  _handleCleverTapEvent(CleverTap.CleverTapInAppNotificationButtonTapped, event);
-});
-
-function _handleCleverTapEvent(eventName, event) {
-console.log('CleverTap Event called - ', eventName, event);
-}
+CleverTap.addListener(CleverTap.CleverTapPushNotificationClicked, (e)=>{console.log(e)});
+console.log("test")
 // Listener to handle incoming deep links
 Linking.addEventListener('url', (e)=>{console.log("Deeplink inside app"+e.url)});
 
@@ -67,46 +47,107 @@ CleverTap.getInitialUrl((err, url) => {
       console.log('CleverTap launch url', err);
   }
 });
-export default class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      images: [
-        "https://source.unsplash.com/1024x768/?nature",
-        "https://source.unsplash.com/1024x768/?water",
-        "https://source.unsplash.com/1024x768/?girl",
-        "https://source.unsplash.com/1024x768/?tree",
-      ]
-    };
-  }
-  render() {
-    return (
-        <View style={{ flex: 1,  }}>
-          <Button onPress={() => {
+
+
+//Initialize app inbox
+CleverTap.initializeInbox();
+
+CleverTap.addListener(CleverTap.CleverTapInboxDidInitialize, (event) => {
+  console.log('CleverTap Inbox Event - ', CleverTap.CleverTapInboxDidInitialize, event);
+});
+// Triggered when a new message is recieved or the app inbox message is read 
+CleverTap.addListener(CleverTap.CleverTapInboxMessagesDidUpdate, (event) => {
+  console.log('CleverTap Inbox Event - ', CleverTap.CleverTapInboxMessagesDidUpdate, event);
+  console.log("INBOX Updated")
+});
+
+
+
+
+CleverTap.createNotificationChannel("abtest", "CT Channel id- abtest", "CT React Native Testing", 5, true) 
+/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
+ * LTI update could not be added via codemod */
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <Button onPress={() => {
             CleverTap.recordEvent('Abeezergetmsg');
             console.log("test");
           }} title="Get event"/> 
-          <Button onPress={() => {
-CleverTap.showInbox({
-    });
+        <Button onPress={() => {
+        CleverTap.showInbox({
+            });
           }} title="App inbox"/> 
-          <Button onPress={() => {
-            this.state.images = []
-             CleverTap.getAllDisplayUnits((err, res) => {
-              this.setState({ datasource: JSON.parse(res) })
-              for (var i=0; i< this.state.datasource.content.length; i++){
-                this.setState({nativekey: this.state.datasource.wzrk_id}) //Store wzrk_id to use it as Unit_id  
-                this.setState({ url: this.state.datasource.content[i].media.url})
-                console.log(i);
-                // this.state.images = [...prevState.images,this.state.url]
-                this.setState({images: this.state.images.concat(this.state.url)})
-                console.log("Unit ID is"+this.state.url)
-              }
-              });
-          }} title="Get display units"/> 
-  
-      </View>
-    );
-}
-}
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
