82d1ad2049fe3c03112ea9c6c3ecc70f1ff67434
abeezer <abeezergolwala98@gmail.com>
2023-02-02 21:16:02 +0530

    App Inbox commit

---
+++ android/app/src/main/java/com/clevertapreactnative/MyFcmMessageListenerService.java
@@ -0,0 +1,64 @@
+package com.clevertapreactnative;
+//import com.clevertap.android.sdk.pushnotification.fcm.CTFcmMessageHandler;
+//import com.google.firebase.messaging.FirebaseMessagingService;
+//import com.google.firebase.messaging.RemoteMessage;
+//
+//public class MyFcmMessageListenerService extends FirebaseMessagingService {
+//    @Override
+//    public void onMessageReceived(RemoteMessage message){
+//        new CTFcmMessageHandler().createNotification(this, message);
+//    }
+//}
+
+
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+import androidx.core.app.NotificationCompat;
+
+import com.clevertap.android.sdk.CleverTapAPI;
+import com.clevertap.android.sdk.CleverTapInstanceConfig;
+import com.clevertap.android.sdk.pushnotification.LaunchPendingIntentFactory;
+import com.clevertap.android.sdk.pushnotification.NotificationInfo;
+//import com.clevertap.android.sdk.pushnotification.fcm.CTFcmMessageHandler;
+import com.clevertap.android.sdk.pushnotification.fcm.CTFcmMessageHandler;
+//import com.clevertap.android.xps.CTXiaomiMessageHandler;
+import com.clevertap.android.sdk.pushnotification.fcm.FcmMessageListenerService;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+//import com.xiaomi.mipush.sdk.MiPushMessage;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class MyFcmMessageListenerService extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(String token1)
+    {
+        super.onNewToken(token1);
+        Log.d("MY_TOKEN", "Refreshed token: " + token1);
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage message) {
+        super.onMessageReceived(message);
+        new CTFcmMessageHandler().createNotification(getApplicationContext(), message);
+
+    }
+
+}
